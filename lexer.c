74889a55658c9f111db8edf29fbd8dbbcab21d10
akankshya107 <akankshyam@gmail.com>
2019-02-27 23:13:47 +0530

    All

--- lexer.c
+++ lexer.c
@@ -5,6 +5,11 @@ TRANSITION_TABLE_ELEM **transition_table;
 void populate_transition_table(){
     TRANSITION_TABLE_ELEM **transition_table = (TRANSITION_TABLE_ELEM**)malloc(sizeof(TRANSITION_TABLE_ELEM*)*sizeof(NO_OF_STATES));
     for(int i=0; i<NO_OF_STATES; i++){
-        transition_table[i]=(TRANSITION_TABLE_ELEM*)malloc(sizeof(TRANSITION_TABLE_ELEM)*(eps+1));
+        transition_table[i]=(TRANSITION_TABLE_ELEM*)malloc(sizeof(TRANSITION_TABLE_ELEM)*(127));
     }
+}
+
+FILE *getStream(FILE *fp){
+    size_t no_of_bytes_read = fread(input_buffer, sizeof(char), 512, fp);
+    return fp;
 }
