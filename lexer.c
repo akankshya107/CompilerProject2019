58334b8c74a6cd67941d47b4a236f1b4aaf5c3b5
bhavananarapareddy <bhavananarapareddy.1299@gmail.com>
2019-02-28 06:32:27 +0530

    State 0

--- lexer.c
+++ lexer.c
@@ -7,6 +7,162 @@ void populate_transition_table(){
     for(int i=0; i<NO_OF_STATES; i++){
         transition_table[i]=(TRANSITION_TABLE_ELEM*)malloc(sizeof(TRANSITION_TABLE_ELEM)*(127));
     }
+    int j;
+    for(j=0;j<=127;j++)
+    {
+        switch(j)
+        {
+            case ',':{
+                transition_table[0][j].u.state=1;
+                transition_table[0][j].flag=0;
+                break;
+            }
+            case ';':{
+                transition_table[0][j].u.state=2;
+                transition_table[0][j].flag=0;
+                break;
+            }
+            case ':':{
+                transition_table[0][j].u.state=3;
+                transition_table[0][j].flag=0;
+                break;
+            }
+            case '.':{
+                transition_table[0][j].u.state=4;
+                transition_table[0][j].flag=0;
+                break;
+            }
+            case '(':{
+                transition_table[0][j].u.state=5;
+                transition_table[0][j].flag=0;
+                break;
+            }
+            case ')':{
+                transition_table[0][j].u.state=6;
+                transition_table[0][j].flag=0;
+                break;
+            }
+            case '=':{
+                transition_table[0][j].u.state=7;
+                transition_table[0][j].flag=0;
+                break;
+            }
+            case '~':{
+                transition_table[0][j].u.state=9;
+                transition_table[0][j].flag=0;
+                break;
+            }
+            case '<':{
+                transition_table[0][j].u.state=10;
+                transition_table[0][j].flag=0;
+                break;
+            }
+            case '>':{
+                transition_table[0][j].u.state=16;
+                transition_table[0][j].flag=0;
+                break;
+            }
+            case '!':{
+                transition_table[0][j].u.state=19;
+                transition_table[0][j].flag=0;
+                break;
+            }
+            case '[':{
+                transition_table[0][j].u.state=21;
+                transition_table[0][j].flag=0;
+                break;
+            }
+            case ']':{
+                transition_table[0][j].u.state=22;
+                transition_table[0][j].flag=0;
+                break;
+            }
+            case '+':{
+                transition_table[0][j].u.state=23;
+                transition_table[0][j].flag=0;
+                break;
+            }
+            case '-':{
+                transition_table[0][j].u.state=24;
+                transition_table[0][j].flag=0;
+                break;
+            }
+            case '*':{
+                transition_table[0][j].u.state=25;
+                transition_table[0][j].flag=0;
+                break;
+            }
+            case '/':{
+                transition_table[0][j].u.state=26;
+                transition_table[0][j].flag=0;
+                break;
+            }
+            case '@':{
+                transition_table[0][j].u.state=29;
+                transition_table[0][j].flag=0;
+                break;
+            }
+
+            case '&':{
+                transition_table[0][j].u.state=32;
+                transition_table[0][j].flag=0;
+                break;
+            }
+            case '#':{
+                transition_table[0][j].u.state=41;
+                transition_table[0][j].flag=0;
+                break;
+            }
+            case '_':{
+                transition_table[0][j].u.state=44;
+                transition_table[0][j].flag=0;
+                break;
+            }
+            case '%':{
+                transition_table[0][j].u.state=53;
+                transition_table[0][j].flag=0;
+                break;
+            }
+            
+            default:
+            {
+                if(j>='b'&&j<='d')
+                {
+                    transition_table[0][j].u.state=35;
+                    transition_table[0][j].flag=0;
+                }
+
+                else if(j=='a'||(j>'d'&&j<='z'))
+                {
+                    transition_table[0][j].u.state=39;
+                    transition_table[0][j].flag=0;
+                }
+
+                else if(j>='0'&&j<='9')
+                {
+                    transition_table[0][j].u.state=48;
+                    transition_table[0][j].flag=0;
+                }
+                else if(j==9||j==10||j==13||j==32)
+                {
+                    transition_table[0][j].u.state=27;
+                    transition_table[0][j].flag=0;
+                }
+
+                else
+                {
+                    transition_table[0][j].u.error_function=symbol_not_found;
+                    transition_table[0][j].flag=0;
+                }
+        
+            }
+            
+
+        }
+    }
+
+
+
 }
 
 FILE *getStream(FILE *fp){
--- lexer.h
+++ lexer.h
@@ -4,3 +4,4 @@ FILE *getStream(FILE *fp);
 tokenInfo* getNextToken();
 void removeComments(char *testcaseFile, char *cleanFile); //char* refers to filename which is opened within the function
 void populate_transition_table();
+void symbol_not_found();
