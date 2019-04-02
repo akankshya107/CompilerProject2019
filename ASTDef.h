200d123e5e6343cc84e4e1f6aa8e0641699c0fee
akankshya107 <akankshyam@gmail.com>
2019-04-02 19:42:35 +0530

    The real AST Node structure

---
+++ ASTDef.h
@@ -0,0 +1,15 @@
+#include "parserDef.h"
+
+typedef struct ASTNodeIt ASTNodeIt;
+typedef enum{}TAG;
+typedef struct ASTNode{
+    TAG tag;
+    tokenInfo *leaf_symbol;
+    ASTNodeIt *parent;
+    ASTNodeIt *children;
+}ASTNode;
+
+typedef struct ASTNodeIt{
+    ASTNode *node;
+    ASTNodeIt *next;
+}ASTNodeIt;
