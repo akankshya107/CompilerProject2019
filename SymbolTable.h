e055d59fb280c58d9b7bb8247ae93fed8d12a29c
akankshya107 <akankshyam@gmail.com>
2019-04-07 22:52:40 +0530

    Preorder implementation and print symbol table full

---
+++ SymbolTable.h
@@ -0,0 +1,14 @@
+// GROUP 39
+// AKANKSHYA MISHRA 2016A7PS0026P
+// NARAPAREDDY BHAVANA 2016A7PS0034P
+// KARABEE BATTA 2016A7PS0052P
+// AASTHA KATARIA 2016A7PS0062P
+#include "SymbolTableDef.h"
+//hashtable functions
+int hashEle(char *str);
+void insertIntoHTEle(hash_ele *elem, hashTable HT);
+hash_ele *create_hashEle(Element *ele, char* str);
+hash_ele* lookupEle(char *str, hashTable HT);
+
+//semantic analyzer functions
+ASTNodeIt* populateSymbolTable(ASTNodeIt* root);
---
+++ SymbolTableDef.h
@@ -0,0 +1,44 @@
+// GROUP 39
+// AKANKSHYA MISHRA 2016A7PS0026P
+// NARAPAREDDY BHAVANA 2016A7PS0034P
+// KARABEE BATTA 2016A7PS0052P
+// AASTHA KATARIA 2016A7PS0062P
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "AST.h"
+#define LEN_HT 41
+typedef enum{ INT, REAL, RECORD }TYPE;
+
+typedef struct Ele{
+  ASTNodeIt *node;
+  struct Ele *next;
+}Ele;
+#define ELEM
+#include "stack.h"
+
+typedef struct Element Element;
+typedef struct hT{
+    char *str;
+    Element *ele;
+    struct hT *next;
+}hash_ele;
+
+typedef hash_ele **hashTable;
+hashTable globalSymbolTable;
+hashTable SymbolTable;
+
+typedef struct symTableElem{
+    TYPE type;
+    int width;
+    int offset;
+    struct symTableElem *typeList; //Only exists for TYPE==RECORD
+}symTableElem;
+
+typedef struct Element{
+    bool is_func;
+    union{
+        hashTable SymbolTable;
+        symTableElem *s;
+    }u;
+}Element;
